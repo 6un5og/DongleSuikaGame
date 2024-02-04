c3d9ae92ea0499aaff85061345c3e46e4446e65b
6un5og <koreabosung@naver.com>
2024-02-04 19:42:28 +0900

    Build APK Game
    build Game simply

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -1,15 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
+    public GameManager manager;
+    public ParticleSystem effect;
     public bool isDrag;
-    Rigidbody2D rigid;
+    public int level;
+    public bool isMerge;
+    public bool isAttach;
+
+    public Rigidbody2D rigid;
+    CircleCollider2D circle;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    float deadTime;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnEnable()
+    {
+        anim.SetInteger("Level", level);
+    }
+
+    void OnDisable()        // 오브젝트 풀링 비활성화 시 재사용을 위해 원래 가지고 있는 모든 정보를 초기화할 함수 필요 (자동 사용 됨)
+    {
+        // 동글 속성 초기화
+        level = 0;
+        isDrag = false;
+        isMerge = false;
+        isAttach = false;
+
+        // 동글 트랜스폼 초기화
+        transform.localPosition = Vector3.zero;             // 동글이 모두 동글 그룹 안에 들어가 있기 때문에 position이 아닌 localPosition
+        transform.localRotation = quaternion.identity;      // rotation은 쿼터니언, 쿼터니언 초기화는 identity
+        transform.localScale = Vector3.zero;                // 레벨에 비례해 크기가 다르기 떄문에 크기도 초기화
+
+        // 동글 물리 초기화
+        rigid.simulated = false;                // 프리펩에서 껐던거처럼 꺼주기
+        rigid.velocity = Vector2.zero;          // 리지드바디2D 쓰기 때문에 Vector2 사용
+        rigid.angularVelocity = 0;              // 회전속도값은 float값이기 때문에 0
+        circle.enabled = true;                  // 숨겨진 동글은 circleCollider 비활성화 했었기 떄문에 활성화해주기
     }
 
     // Update is called once per frame
@@ -46,5 +86,152 @@ public class Dongle : MonoBehaviour
     {
         isDrag = false;
         rigid.simulated = true;         // 손을 뗐을 때 물리적용 받게 하기
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(AttachRoutine());
+    }
+
+    IEnumerator AttachRoutine()
+    {
+        if (isAttach)
+        {
+            yield break;    // 코루틴에서 탈출은 return 이 아닌 break
+        }
+
+        isAttach = true;
+
+        manager.SfxPlay(GameManager.Sfx.Attach);        // 충돌음의 주기
+
+        yield return new WaitForSeconds(0.2f);
+        isAttach = false;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)       // 물리적 충돌중일 때 계속 실행되는 함수
+    {
+        if (collision.gameObject.tag == "Dongle")       // 부딪히는 오브젝트의 태그가 동글일 때
+        {
+            Dongle other = collision.gameObject.GetComponent<Dongle>();     // 오브젝트 동글의 컴포넌트 가져오기
+
+            // 조건 4개 모두 충족 (본인 오브젝트의 레벨과 충돌한 오브젝트의 레벨이 같을 시, 내가 합치고 있지 않을 시, 상대 오브젝트가 합치고 있지 않을 시, 레벨이 7보다 작아야 함)
+            if (level == other.level && !isMerge && !other.isMerge && level < 7)
+            {
+                // 나와 상대편 위치 가져오기
+                float meX = transform.position.x;
+                float meY = transform.position.y;
+                float otherX = other.transform.position.x;
+                float otherY = other.transform.position.y;
+                // 1. 내가 아래에 있을 때
+                // 2. 동일한 높이일 때, 내가 오른쪽에 있을 때
+                if (meY < otherY || (meY == otherY && meX > otherX))        // 동일한 높이 + 오른쪽에 있다 두 조건을 묶는 소괄호 추가
+                {
+                    // 나는 레벨업하고 상대방은 숨기기
+                    other.Hide(transform.position);     // 상대방이 내쪽으로 와서 합치기 때문에 변수에 내 위치 입력
+
+                    LevelUp();
+                }
+            }
+        }
+    }
+
+    public void Hide(Vector3 targetPos)
+    {
+        isMerge = true;
+
+        rigid.simulated = false;
+        circle.enabled = false;
+
+        if (targetPos == Vector3.up * 100)          // 게임 오버에도 이펙트가 나오도록 로직 추가
+        {
+            EffectPlay();
+        }
+
+        StartCoroutine(HideRoutine(targetPos));
+    }
+
+    IEnumerator HideRoutine(Vector3 targetPos)      // 이동하는 코루틴, 성장하는 상대에게 이동하므로 Vector3 매개변수 추가
+    {
+        int frameCount = 0;
+
+        while (frameCount < 20)         // while문을 update처럼 로직을 실행하여 이동
+        {
+            frameCount++;
+            if (targetPos != Vector3.up * 100)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            }
+            else if (targetPos == Vector3.up * 100)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, 0.2f);  // 스케일 없애기
+            }
+
+            yield return null;          // 한 프레임씩 끊어주기
+        }
+
+        manager.score += (int)Mathf.Pow(2, level);      // Mathf.Pow는 float형 변환이기 때문에 명시적으로 변환(int)
+
+        isMerge = false;                // 잠금 해제
+        gameObject.SetActive(false);    // 숨기기
+    }
+
+    void LevelUp()
+    {
+        isMerge = true;
+
+        // 레벨업 중에 방해가 될 수 있는 물리속도 제거
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0;
+
+        StartCoroutine(LevelUpRoutine());
+    }
+
+    IEnumerator LevelUpRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        anim.SetInteger("Level", level + 1);        // 변수 값에 ++level을 해버리면 애니메이션 시간이 부족함
+        EffectPlay();
+        manager.SfxPlay(GameManager.Sfx.LevelUp);
+
+        yield return new WaitForSeconds(0.3f);
+        level++;
+
+        manager.maxLevel = Mathf.Max(level, manager.maxLevel);  // 인자값 중 최대값을 반환해주는 함수(순서 상관 없음)
+
+        isMerge = false;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish")
+        {
+            deadTime += Time.deltaTime;         // 각 프레임의 시간을 계속 더함
+
+            if (deadTime > 2)
+            {
+                spriteRenderer.color = new Color(0.9f, 0.2f, 0.2f);
+            }
+            if (deadTime > 5)
+            {
+                manager.GameOver();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish")
+        {
+            deadTime = 0;
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void EffectPlay()           // 파티클 위치와 크기 보정 함수
+    {
+        effect.transform.position = transform.position;
+        effect.transform.localScale = transform.localScale;
+        effect.Play();          // 파티클 함수 Play() 로 실행
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;              // 장면 관리 담당
 
 public class GameManager : MonoBehaviour
 {
-    public Dongle lastDongle;
+    [Header("------------[ Core ]")]
+    public int score;
+    public int maxLevel;
+    public bool isOver;
+
+    [Header("------------[ Object Pooling ]")]
     public GameObject donglePrefab;
     public Transform dongleGroup;
+    public List<Dongle> donglePool;
 
-    void Start()
+    public GameObject effectPrefab;
+    public Transform effectGroup;
+    public List<ParticleSystem> effectPool;
+
+    [Range(1, 30)]
+    public int poolSize;        // 오브젝트풀 관리를 위해 사이즈, 커버 변수 추가
+    public int poolCursor;
+    public Dongle lastDongle;
+
+    [Header("------------[ Audio ]")]
+
+    public AudioSource bgmPlayer;
+    public AudioSource[] sfxPlayer;
+    public AudioClip[] sfxClip;
+    public enum Sfx { LevelUp, Next, Attach, Button, Over };
+    int sfxCursor;
+
+    [Header("------------[ UI ]")]
+    public GameObject startGroup;
+    public GameObject endGroup;
+    public Text scoreText;
+    public Text maxScoreText;
+    public Text subScoreText;
+
+    [Header("------------[ ETC ]")]
+    public GameObject line;
+    public GameObject bottom;
+
+    void Awake()
     {
-        NextDongle();
+        Application.targetFrameRate = 60;           // 프레임 고정
+
+        donglePool = new List<Dongle>();
+        effectPool = new List<ParticleSystem>();
+
+        for (int index = 0; index < poolSize; index++)      // for 문으로 지정해놓은 poolSize만큼 동글 생성
+        {
+            MakeDongle();
+        }
+        if (!PlayerPrefs.HasKey("MaxScore"))
+        {
+            PlayerPrefs.SetInt("MaxScore", 0);
+        }
+
+        maxScoreText.text = PlayerPrefs.GetInt("MaxScore").ToString();
+    }
+
+    public void GameStart()
+    {
+        line.SetActive(true);
+        bottom.SetActive(true);
+        scoreText.gameObject.SetActive(true);
+        maxScoreText.gameObject.SetActive(true);
+        startGroup.SetActive(false);
+
+        bgmPlayer.Play();
+        SfxPlay(Sfx.Button);
+        Invoke("NextDongle", 1.5f);   // 함수 호출에 딜레이를 주고싶을 때 사용하는 함수
+    }
+
+    Dongle MakeDongle()
+    {
+        // 이펙트 생성
+        GameObject instantEffectObj = Instantiate(effectPrefab, effectGroup);
+        instantEffectObj.name = "Effect" + effectPool.Count;        // 이펙트 오브젝트가 생성 될 때 이름 지정
+        ParticleSystem instantEffect = instantEffectObj.GetComponent<ParticleSystem>(); // 오브젝트 컴포넌트 초기화
+        effectPool.Add(instantEffect);      // List.Add : 해당 리스트에 데이터를 추가하는 함수
+
+        // 동글 생성
+        GameObject instantDongleObj = Instantiate(donglePrefab, dongleGroup);    // Instantiate 함수 그대로 써도 가능하지만 두번째 인자값이 생성할 때 지정되는 부모 오브젝트
+        instantDongleObj.name = "Dongle" + donglePool.Count;
+        Dongle instantDongle = instantDongleObj.GetComponent<Dongle>();
+        instantDongle.manager = this;               // 생성된 동글 오브젝트의 매니저 초기화
+        instantDongle.effect = instantEffect;        // 동글 생성하면서 바로 이펙트 변수를 생성했던 것으로 초기화
+        donglePool.Add(instantDongle);
+        return instantDongle;       // 반환값 동글
     }
 
     Dongle GetDongle()
     {
-        GameObject instant = Instantiate(donglePrefab, dongleGroup);    // Instantiate 함수 그대로 써도 가능하지만 두번째 인자값이 생성할 때 지정되는 부모 오브젝트
-        Dongle instantDongle = instant.GetComponent<Dongle>();
+        for (int index = 0; index < donglePool.Count; index++)
+        {
+            poolCursor = (poolCursor + 1) % donglePool.Count;
+            if (!donglePool[poolCursor].gameObject.activeSelf)          // 현재 리스트 위치에 있는 오브젝트가 비활성화 되었는지 확인
+            {
+                return donglePool[poolCursor];      // 탐색하여 만난 오브젝트가 비활성화라면 반환
+            }
+        }
 
-        return instantDongle;
-
+        return MakeDongle();    // 모든 것이 활성화중(사용중) 이라면 return에 생성 함수 반환, 해당 함수에 이미 반환이 되고 있으므로 가능
     }
 
     void NextDongle()
     {
-        Dongle newDongle = GetDongle();
-        lastDongle = newDongle;         // 동글 생성 시 보관 용도로 동글 변수에 저장
+
+        if (isOver)
+        {
+            return;
+        }
+
+        lastDongle = GetDongle();         // 동글 생성 시 보관 용도로 동글 변수에 저장
+        lastDongle.level = Random.Range(0, maxLevel);  // maxRange는 포함이 안되기 때문에 7까지 하고싶으면 8로 설정
+        lastDongle.gameObject.SetActive(true);  // 프리팹 active 꺼놓기
+
+        SfxPlay(Sfx.Next);
+        StartCoroutine(WaitNext());
+    }
+
+    IEnumerator WaitNext()              // 동글이 비워질 때 까지 기다리는 코루틴 생성
+    {
+        while (lastDongle != null)      // 동글이 비워질 때까지 무한루프 생성
+        {
+            yield return null;          // yield 없이 돌리면 무한루프에 빠져 유니티가 멈춤 
+        }
+
+        yield return new WaitForSeconds(2.5f);
+
+        NextDongle();                   // 기다린 후에 다음 동글 실행
     }
 
     public void TouchDown()
@@ -43,5 +151,98 @@ public class GameManager : MonoBehaviour
 
         lastDongle.Drop();
         lastDongle = null;              // 손을 떼면 더 이상 터치 못하게 막기
+    }
+
+    public void GameOver()
+    {
+        if (isOver)
+        {
+            return;
+        }
+        isOver = true;
+
+        StartCoroutine(GameOverRoutine());
+
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        // 1. 장면 안에 활성화 되어있는 모든 동글 가져오기
+        Dongle[] dongles = FindObjectsOfType<Dongle>();         // FindObjectsOfType<T> : 장면에 올라온 T 컴포넌트들을 탐색
+
+        // 2. 지우기 전에 모든 동글의 물리효과 비활성화
+        for (int index = 0; index < dongles.Length; index++)
+        {
+            dongles[index].rigid.simulated = false;
+        }
+
+
+        // 3. 1번의 목록을 하나씩 접근해서 지우기
+        for (int index = 0; index < dongles.Length; index++)
+        {
+            dongles[index].Hide(Vector3.up * 100);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        int maxScore = Mathf.Max(score, PlayerPrefs.GetInt("MaxScore"));    // 최고점수 비교
+        PlayerPrefs.SetInt("MaxScore", maxScore);       // 최고점수 업데이트
+
+        subScoreText.text = "점수 : " + scoreText.text;
+        endGroup.SetActive(true);
+
+        bgmPlayer.Stop();       // 게임 종료되면 배경음악도 멈추도록 Stop함수 호출
+        SfxPlay(Sfx.Over);
+    }
+
+    public void Reset()
+    {
+        SfxPlay(Sfx.Button);
+        StartCoroutine(ResetCoroutine());
+    }
+
+    IEnumerator ResetCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void SfxPlay(Sfx type)
+    {
+        switch (type)
+        {
+            case Sfx.LevelUp:
+                sfxPlayer[sfxCursor].clip = sfxClip[Random.Range(0, 3)];
+                break;
+            case Sfx.Next:
+                sfxPlayer[sfxCursor].clip = sfxClip[3];
+                break;
+            case Sfx.Attach:
+                sfxPlayer[sfxCursor].clip = sfxClip[4];
+                break;
+            case Sfx.Button:
+                sfxPlayer[sfxCursor].clip = sfxClip[5];
+                break;
+            case Sfx.Over:
+                sfxPlayer[sfxCursor].clip = sfxClip[6];
+                break;
+        }
+
+        sfxPlayer[sfxCursor].Play();
+        sfxCursor = (sfxCursor + 1) % sfxPlayer.Length;         // 일방적으로 인덱스 숫자를 올리면 OutOfRange 에러 발생
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void LateUpdate()
+    {
+        scoreText.text = score.ToString();
     }
 }
